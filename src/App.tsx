403785b4b21f5b412c320eb05c797154b2ac3ce8
Aryan Prince <aryanprincesanty@icloud.com>
2023-02-27 22:51:42 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-[400px] w-[600px] bg-[#262626] text-white">
+      <div className="m-4 flex flex-col gap-4">
+        <p className="text-2xl font-semibold">Sample Extension</p>
+        {/* Additional code goes here */}
+      </div>
+    </div>
+  )
+}
+
+export default App
